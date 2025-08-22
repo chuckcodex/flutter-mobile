e0266bd7ba749035634f21ce068b97cfd6002e83
codeduck <charlescollantes.dev@gmail.com>
2025-08-22 08:16:30 +0800

    second commit

--- zakaz/lib/main.dart
+++ zakaz/lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'widgets/login.dart';
 
 void main() async {
   // supabase_setup
 
-  widgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
     url: 'https://feaqyfnmhnolwjrtxzff.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlYXF5Zm5taG5vbHdqcnR4emZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxMjU0MzcsImV4cCI6MjA3MDcwMTQzN30.JYAx_-xKxM1QEZZGbnd_MMfiiFpqm2Tvy1tS_ijn3p8',
+  );
 
   //supabase_setup
-  runApp(ZakazApp());
+  runApp(const ZakazApp());
 }
 
 class ZakazApp extends StatelessWidget {
